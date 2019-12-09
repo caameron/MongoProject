26edd83a5bb8e29b8071d7ce3a9854dfbbabb565
Caameron <caameron@pdx.edu>
2019-12-09 01:52:27 +0000

    Finihsed Query

--- query4.py
+++ query4.py
@@ -17,7 +17,22 @@ end1 = datetime(2011, 9, 22, 9, 0, 0)
 start2 = datetime(2011, 9, 22, 16, 0, 0)
 end2 = datetime(2011, 9, 22, 18, 0, 0)
 
-count1 = db.allLoopData.find({'stationid': "1047", 'starttime': {'$gte': start1, '$lt': end1}}).count()
-print(count1)
-count2 = db.allLoopData.find({'stationid': "1047", 'starttime': {'$gte': start2, '$lt': end2}}).count()
-print(count2)
+#Aggregations to sum all speeds and average it out
+sumSpeeds1 = [ {'$match': {'stationid':1047, 'starttime': {'$gte': start1, '$lt': end1}}}, {'$group': {'_id': 1, 'totalSpeed': {'$sum': '$speed'}, 'count': {'$sum': 1}, 'avg': {'$avg': '$speed'}}} ]
+sumSpeeds2 = [ {'$match': {'stationid':1047, 'starttime': {'$gte': start2, '$lt': end2}}}, {'$group': {'_id': 1, 'totalSpeed': {'$sum': '$speed'}, 'count': {'$sum': 1}, 'avg': {'$avg': '$speed'}}} ]
+
+#Query Database to get total speed, count, and average speed for those hours
+count1 = list(db.loopdata.aggregate(sumSpeeds1))
+count2 = list(db.loopdata.aggregate(sumSpeeds2))
+
+#Print out values
+totalSpeed = count1[0]['totalSpeed'] + count2[0]['totalSpeed']
+totalCount = count1[0]['count'] + count2[0]['count']
+avgSpeed = (count1[0]['avg'] + count2[0]['avg'])/2
+print "Total Speed:", totalSpeed
+print "Total Count:", totalCount
+print "Average Speed:", avgSpeed
+
+#Calculate and print out average travel time
+travelTime = 1.6/avgSpeed * 3660
+print "Average Travel Time:", travelTime
